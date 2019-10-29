cd2fda3049c54d0749259aaed90d3c5a0aeee0d4
thomas-god <thomas.godlewski@icloud.com>
2019-10-29 22:40:27 +0100

    Add support for multi elements expressions

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import random
 import os
 
-def draw_one_parenthesis():
+def get_one_set():
     """
     Draw parameters defining a parenthesis object (a*xi + b*xib) with
     xib = 1 - xi. a and b are non-zero integers while xi is a boolean 
@@ -26,20 +26,40 @@ def draw_one_parenthesis():
         random.randint(0, 1)
     )
 
-def fmt_parenthesis(par):
+def get_multi_set(n_set_max):
+    """
+    Concatenate up to `n_set_max` (a*xi + b*xib) set and returns a list of tuples.
+    """
+    if n_set_max < 2:
+        raise ValueError('`n_set_max` must be at least 2.')
+    n = random.randint(2, n_set_max)
+    return [get_one_set() for i in range(n)]
+
+def fmt_one_set(in_set):
     """
     Using an (a, b, xi) tuple, returns the coresponding string
     to form (a*xi + b*xib)
     """
     return (
         "("
-        f"{par[0]}"
-        f"{'x' if par[2] else ''}"
-        f"{'+' if par[1] > 0 else ''}"
-        f"{par[1]}"
-        f"{'x' if (1 - par[2]) else ''}"
+        f"{in_set[0]}"
+        f"{'x' if in_set[2] else ''}"
+        f"{'+' if in_set[1] > 0 else ''}"
+        f"{in_set[1]}"
+        f"{'x' if (1 - in_set[2]) else ''}"
         ")"
     )
+
+def fmt_multi_set(mult_set):
+    """
+    Format each element from a list of sets and concatenate them.
+    """
+    str_mult_set = ""
+    for in_set in mult_set:
+        str_mult_set += fmt_one_set(in_set)
+    return str_mult_set
+
+
 
 def get_latex_start():
     """
@@ -50,6 +70,8 @@ def get_latex_start():
         "\\usepackage[T1]{fontenc}\n"
 
         "\\begin{document}\n"
+
+        "\\begin{itemize}\n"
     )
 
 def end_latex_doc(doc):
@@ -59,6 +81,7 @@ def end_latex_doc(doc):
     """
     return (
         doc
+        + "\n\\end{itemize}"
         + "\n\\end{document}"
     )
 
@@ -69,17 +92,16 @@ def add_latex_math(doc, expr):
     """
     return (
         doc 
-        + f"\\[{expr}\\]\n"
+        + f"\\item ${expr}$\n"
     )
 
 
 if __name__ == '__main__':
     doc = get_latex_start()
     
-    doc = add_latex_math(doc, fmt_parenthesis(draw_one_parenthesis()))
-    doc = add_latex_math(doc, fmt_parenthesis(draw_one_parenthesis()))
-    doc = add_latex_math(doc, fmt_parenthesis(draw_one_parenthesis()))
-
+    for i in range(10):
+        doc = add_latex_math(doc, fmt_multi_set(get_multi_set(2)))
+    
     doc = end_latex_doc(doc)
 
     with open('doc.tex', 'w') as f:
