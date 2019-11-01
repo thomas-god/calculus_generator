b6919d99ef445249c955da85a506507bb3f97c27
thomas-god <thomas.godlewski@icloud.com>
2019-11-01 14:04:24 +0100

    Add db checks when generating exercices

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import random
 import os
 import sqlite3
+from gen_db import init_db, insert_parenthesis
 
 def get_one_set():
     """
@@ -24,40 +25,41 @@ def get_one_set():
     conn = sqlite3.connect('test.db')
     c = conn.cursor()
     
-    flag = True
-    while flag:
-        cur_set = (
-            random.sample(set_n, 1)[0],
-            random.sample(set_n, 1)[0],
-            random.randint(0, 1)
-        )
+    cur_set = (
+        random.sample(set_n, 1)[0],
+        random.sample(set_n, 1)[0],
+        random.randint(0, 1)
+    )
 
-        c.execute(f"""
-            SELECT * FROM test
-            WHERE alpha = {cur_set[0]} AND beta = {cur_set[1]} AND xi = {cur_set[2]};
-        """)
-        if len(c.fetchall()) == 0:
-            flag = False
-
-    print(cur_set)
-
-    c.execute("""
-        INSERT INTO test (alpha, beta, xi)
-        VALUES (?,?,?);
-    """, cur_set)
+    idx = insert_parenthesis(c, *cur_set)
 
     conn.commit()
     conn.close()
-    return cur_set
+    return cur_set, idx
 
-def get_multi_set(n_set_max):
+def get_multi_set():
     """
-    Concatenate up to `n_set_max` (a*xi + b*xib) set and returns a list of tuples.
+    Concatenate 2 (a*xi + b*xib) set2 and returns a list of tuples.
     """
-    if n_set_max < 2:
-        raise ValueError('`n_set_max` must be at least 2.')
-    n = random.randint(2, n_set_max)
-    return [get_one_set() for i in range(n)]
+    conn = sqlite3.connect('test.db')
+    c = conn.cursor()
+    random.seed(1234)
+    flag = True
+    while flag:
+        parenthesis = [get_one_set() for i in range(2)]
+        idx_1, idx_2 = parenthesis[0][1], parenthesis[1][1]
+        try:
+            c.execute("INSERT INTO exercices VALUES (?, ?)", (idx_1, idx_2))
+        except sqlite3.IntegrityError:
+            print("Trying to insert a record that already exists.")
+            conn.rollback()
+        else:
+            print("New record successfully into exercices table.")
+            conn.commit()
+            flag = False
+    
+    conn.close()
+    return [parenthesis[0][0], parenthesis[1][0]]
 
 def fmt_one_set(in_set):
     """
@@ -119,38 +121,17 @@ def add_latex_math(doc, expr):
         + f"\\item ${expr}$\n"
     )
 
-def init_db():
-    """
-    If no database exists, create it along with the table 
-    for storing previously generated sets.
-    """
-    conn = sqlite3.connect('test.db')
-    c = conn.cursor()
-
-    c.execute("DROP TABLE IF EXISTS test;")
-    c.execute("""
-        CREATE TABLE IF NOT EXISTS test(
-            alpha TINYINT,
-            beta TINYINT,
-            xi TINYINT,
-            PRIMARY KEY (alpha, beta, xi)
-        );
-    """)
-    conn.commit()
-    conn.close()
-
-
 if __name__ == '__main__':
     init_db()
     
     doc = get_latex_start()
     
     for i in range(10):
-        doc = add_latex_math(doc, fmt_multi_set(get_multi_set(2)))
+        doc = add_latex_math(doc, fmt_multi_set(get_multi_set()))
     
     doc = end_latex_doc(doc)
 
     with open('doc.tex', 'w') as f:
         f.write(doc)
     
-    #os.system("pdflatex doc.tex")
+    os.system("pdflatex doc.tex")
